3396a7a45b8f364a32c0896aa730df167a1ea8b9
leomsa <leo.akimim@gmail.com>
2024-06-18 20:48:26 -0300

    Sets up MongoDB for data persistence
    - Configures MongoDB using Spring Data MongoDB
    - Adds necessary dependencies to pom.xml
    - Implements configuration class for MongoDB connection
    
    Issue #1

---
+++ asian-box/src/test/java/com/leonardo/asian_box/AsianBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.asian_box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsianBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
