0b030518004ed152a2622ea9ecfdafb2dbd18b71
leomsa <leo.akimim@gmail.com>
2024-06-24 20:18:33 -0300

    Changed annotation getter/setter to @Data.
    Issue #7

--- asian-box/src/main/java/com/leonardo/asian_box/entities/category/Category.java
+++ asian-box/src/main/java/com/leonardo/asian_box/entities/category/Category.java
@@ -1,9 +1,7 @@
 package com.leonardo.asian_box.entities.category;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
