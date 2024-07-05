f6563df751ee453f3c8cc63a29b38bda62441635
Leonardo Machado dos Santos <leonardo.machadodossantos@agcocorp.com>
2024-07-05 14:34:56 -0300

    Creating a connection to use mongoDB with atlas + AWS EC2 instace

--- asian-box/src/main/java/com/leonardo/asian_box/config/mongo/MongoDBConfig.java
+++ asian-box/src/main/java/com/leonardo/asian_box/config/mongo/MongoDBConfig.java
@@ -1,5 +1,11 @@
 package com.leonardo.asian_box.config.mongo;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -8,13 +14,31 @@ import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
 @Configuration
 public class MongoDBConfig {
+
+    @Value("${mongodb.apiKey}")
+    private String apiKey;
+
+    @Value("${mongodb.privateKey}")
+    private String privateKey;
+
     @Bean
-    public MongoDatabaseFactory mongoConfigure() {
-        return new SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/product-catalog");
+    public MongoDatabaseFactory mongoDbFactory() {
+        String connectionString = String.format("mongodb+srv://%s:%s@cluster0.hkwjjc0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", apiKey, privateKey);
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        return new SimpleMongoClientDatabaseFactory(MongoClients.create(settings), "product-catalog");
     }
 
     @Bean
     public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoConfigure());
+        return new MongoTemplate(mongoDbFactory());
     }
 }
